c5bea9c1a3e744ab356073c7e68675fb9412e937
Pierre Averous <piaverous@gmail.com>
2020-07-14 14:28:43 +0200

    feat(main): add about listing for drive user and storageQuota

--- main.go
+++ main.go
@@ -10,33 +10,46 @@ import (
 	"google.golang.org/api/drive/v3"
 )
 
+func getDriveService() *drive.Service {
+	ctx := context.Background()
+	client, err := google.DefaultClient(
+		ctx,
+		drive.DriveScope,
+	)
+
+	svc, err := drive.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Drive client: %v", err)
+	}
+	return svc
+}
+
 func main() {
 	if os.Getenv("GOOGLE_APPLICATION_CREDENTIALS") == "" {
 		log.Fatal("Permission Denied - Please export GOOGLE_APPLICATION_CREDENTIALS env variable")
 	}
 
-	ctx := context.Background()
-	client, err := google.DefaultClient(
-		ctx,
-		drive.DriveMetadataReadonlyScope,
-	)
+	svc := getDriveService()
+	r, err := svc.Files.
+		List().
+		PageSize(10).
+		Fields("nextPageToken, files(id, name)").
+		Do()
 
-	srv, err := drive.New(client)
-	if err != nil {
-		log.Fatalf("Unable to retrieve Drive client: %v", err)
-	}
-
-	r, err := srv.Files.List().PageSize(10).
-		Fields("nextPageToken, files(id, name)").Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve files: %v", err)
 	}
+
 	fmt.Println("Files:")
 	if len(r.Files) == 0 {
 		fmt.Println("No files found.")
 	} else {
 		for _, i := range r.Files {
-			fmt.Printf("%s (%s)\n", i.Name, i.Id)
+			fmt.Printf("\t- %s (%s)\n", i.Name, i.Id)
 		}
 	}
+
+	result, err := svc.About.Get().Fields("storageQuota, user").Do()
+	fmt.Printf("%v\n", result.User)
+	fmt.Printf("%v\n", result.StorageQuota)
 }
